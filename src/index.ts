52fc74f058919345441bd858eaaf75849d981676
Nathan Alcantara <nathangabriel97@gmail.com>
2021-04-04 06:09:11 +0000

    splitting server from app

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,9 @@
-import express, { Request, Response } from 'express'
 import Discord, { Message } from 'discord.js'
 import { DISCORD_TOKEN } from './config/secrets'
 import CommandHandler from './commandHandler'
 import config from './config/botConfig'
 
-const PORT = process.env.PORT || 5000
-
-const app = express()
 const client = new Discord.Client()
-
-//////////////////////////////////////////////////////////////////
-//             EXPRESS SERVER SETUP FOR UPTIME ROBOT            //
-//////////////////////////////////////////////////////////////////
-app.use(express.urlencoded({ extended: true }))
-
-app.use('/', (request: Request, response: Response) => {
-  response.sendStatus(200)
-})
 
 const commandHandler = new CommandHandler(config.prefix)
 
@@ -36,4 +23,3 @@ client.on('error', (e) => {
 })
 
 client.login(DISCORD_TOKEN)
-app.listen(PORT, () => console.log(`Server started on port ${PORT}!`))
